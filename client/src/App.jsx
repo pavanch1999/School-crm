16e7901d49b2e1cf1609275485bc6c3b1e3cf8bb
pavanch1999 <55751806+pavanch1999@users.noreply.github.com>
2025-02-21 21:44:03 +0530

    first

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,39 +1,28 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from "react-router-dom";
-import Home from "./pages/Home";
-import Students from "./pages/Students";
-import Teachers from "./pages/Teachers";
-import Classes from "./pages/Classes";
-import Navbar from "./components/Navbar";
-import Login from "./pages/Login";
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Students from './pages/Students.jsx';
+import Teachers from './pages/teachers.jsx';
+import Classes from './pages/Classes';
+import Navbar from './components/Navbar';
+import Login from './pages/Login';
+import { Navigate } from 'react-router-dom';
 function App() {
   return (
     <Router>
-      <MainLayout />
-    </Router>
-  );
-}
-
-function MainLayout() {
-  const location = useLocation();
-  const hideNavbar = location.pathname === "/login";
-
-  return (
-    <>
-      {!hideNavbar && <Navbar />}
+      <Navbar />
       <div className="container mx-auto p-4">
         <Routes>
-          <Route path="/" element={<Navigate to="/login" />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/students" element={<Students />} />
           <Route path="/teachers" element={<Teachers />} />
           <Route path="/classes" element={<Classes />} />
+          
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
         </Routes>
       </div>
-    </>
+    </Router>
   );
 }
-
 export default App;
